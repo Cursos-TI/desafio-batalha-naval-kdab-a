65d3d2bccbb12e9ff424769cc9691c54225eeccf
kdab-a <kdab.augusto@gmail.com>
2025-06-29 19:28:07 +0000

    Desafio Batalha Naval Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    //Declaração e inicialização da matriz, vetores e variáveis:
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-    int navio1[3] = {3, 3, 3}, navio2[3] = {3, 3, 3};
+    //Declaração e inicialização da matriz:
+    int tabuleiro[10][10] = {0};
 
     //Exibição do tabuleiro anterior ao posicionamento dos navios:
     for (int i = 0; i < 10; i++){
@@ -24,13 +12,46 @@ int main() {
         printf("\n");
     }
 
-    //Inserção dos navios e exibição do tabuleiro atualizado:
-    tabuleiro [4][2] = navio1[0];
-    tabuleiro [4][3] = navio1[1];
-    tabuleiro [4][4] = navio1[2];
-    tabuleiro [7][6] = navio2[0];
-    tabuleiro [8][6] = navio2[1];
-    tabuleiro [9][6] = navio2[2];
+    printf("\n");
+
+    //Inserção dos navios:
+    //Navio horizontal:
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j< 10; j++){
+            if (i == 3 && j >= 3 && j <= 5){
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
+
+    //Navio vertical:
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j< 10; j++){
+            if (j == 6 && i >= 7 && i <= 9){
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
+
+    //Navio diagonal 1:
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j< 10; j++){
+            if (i == j && j < 3 && i < 3){
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
+
+    //Navio diagonal 2:
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j< 10; j++){
+            if (i + j == 9 && i < 3 && j <= 9 && j >= 7){
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
+
+    //Exibição do tabuleiro após o posicionamento dos navios:
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 10; j++){
             printf("%d\t", tabuleiro[i][j]);
